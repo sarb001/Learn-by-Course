e700b3af493a36f6fde8cdc1b9d6dd1446e54bac
SARB <mrsinghbusiness05@gmail.com>
2023-04-26 16:49:46 +0530

    frontend Files Structured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+       <Router>
+          <Routes>
+              <Route exact path = "/"       element = {<Home />}>  </Route>
+              <Route  path = "/courses"     element = {<Home />}>  </Route>
+              <Route  path = "/course/:id"  element = {<Home />}>  </Route>
+
+              <Route exact path = "/login"  element = {</> }>  </Route>
+              <Route exact path = "/login"  element = {</> }>  </Route>
+              <Route exact path = "/login"  element = {</> }>  </Route>
+              <Route exact path = "/profile"  element = {</> }>  </Route>
+             
+             
+              <Route exact path = "/profile"  element = {</> }>  </Route>
+              <Route exact path = "/profile"  element = {</> }>  </Route>
+              <Route exact path = "/profile"  element = {</> }>  </Route>
+              <Route exact path = "/register"  element = {</> }>  </Route>
+
+
+              <Route exact path = "/forgetpassword"  element = {</> }>  </Route>
+              <Route exact path = "/resetpassword/:token"  element = {</> }>  </Route>
+
+
+              <Route exact path = "/subscribe"  element = {</> }>  </Route>
+              <Route exact path = "/resetpassword/:token"  element = {</> }>  </Route>
+              <Route exact path = "/resetpassword/:token"  element = {</> }>  </Route>
+              <Route exact path = "/paymentfail"  element = {</> }>  </Route>
+              
+              
+              <Route exact path = "/admin/dashboard"  element = {</> }>  </Route>
+             
+              <Route exact path = "/admin/createcourse"  element = {</> }>  </Route>
+              
+              <Route exact path = "/admin/courses"  element = {</> }>  </Route>
+              
+              <Route exact path = "/admin/users"  element = {</> }>  </Route>
+
+
+          </Routes>
+       </Router>
     </div>
   );
 }
