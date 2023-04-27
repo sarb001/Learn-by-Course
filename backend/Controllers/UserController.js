3cb2cddb7b63fed254e11dac717ad967cf8048e0
SARB <mrsinghbusiness05@gmail.com>
2023-04-27 19:44:27 +0530

    cookies ( token inc.) stored

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import { User } from '../Models/User.js';
+import  bcrypt  from 'bcrypt';
+
+import jwt  from "jsonwebtoken";
 
 export const register = async(req,res,next) => {
    
         const { name,email , password } = req.body;
-        try{
-            
+        try{  
             if(!name || !email || !password){
                 return res.json({message : " Please Fill All the Fields "})
             }
@@ -13,13 +15,35 @@ export const register = async(req,res,next) => {
             if(user){
                 return res.json({message : ' user Already Existed '})
             }
+            
+            let hashpass = await bcrypt.hash(password,10)
+           
             const createuser = await User.create({
-                name,email ,password
+                name,
+                email,
+                password : hashpass
             })
+            console.log(' Hash Pass is  --',hashpass);
             console.log('created user is --',createuser);
 
-           return  res.status(201).json({
-                message : " User Created  "
+            var token = jwt.sign({_id : createuser._id},'ekekkkeke' , {
+                    expiresIn : '1d',
+            })
+
+            console.log('token is - ',token);
+
+            // res.cookie()
+
+         const options = {
+            expires : new Date(Date.now()),
+            httpOnly : true,
+            secure : true,
+         }
+
+
+           return  res.status(201).cookie("token" , token , options).json({
+                message : " User Created Now",
+                token,
             })
 
         }catch(error){
--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import user from './Routes/UserRoutes.js';
 import { connectdb } from './Config/Database.js';
 
+import cookieparser from  'cookie-parser';
+
 const app = express();
 
 dotenv.config({
@@ -12,6 +14,7 @@ dotenv.config({
 connectdb();
 const PORT = process.env.PORT 
 
+app.use(cookieparser());
 app.use(express.json())
 
 app.use('/api/v1',user);
