ad4286db955efbae42df31abf7fd0972c8512b09
SARB <mrsinghbusiness05@gmail.com>
2023-05-01 04:24:47 +0530

    Build / Deployed /Frontend+Backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import user from './Routes/UserRoutes.js';
-import course from './Routes/CourseRoutes.js';
-
 import { connectdb } from './Config/Database.js';
-
+import course from './Routes/CourseRoutes.js';
 import cookieparser from  'cookie-parser';
+
+import cors from 'cors';
+
 
 const app = express();
 
 dotenv.config({
     path:'./Config/config.env'
 })
+
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true,
+    methods : ["GET","POST","PUT","DELETE"]
+}))
 
 connectdb();
 const PORT = process.env.PORT 
@@ -25,4 +32,11 @@ app.use('/api/v1',course);
 
 app.listen(PORT,() => {
     console.log(` Server is Running ${PORT} Brooo.. `);
+})
+
+
+app.get('/' , (req,res)  => {
+     res.send(`<h2> Server is Working 
+     <a href = ${process.env.FRONTEND_URL}>  CLick herer  </a>
+     </h2>`)
 })
