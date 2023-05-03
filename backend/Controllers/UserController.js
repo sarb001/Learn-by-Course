a34f22fb8b59341a402aeca9ff5e496d1c1ba7d2
SARB <mrsinghbusiness05@gmail.com>
2023-05-03 05:31:05 +0530

    cors options Corrected

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -35,6 +35,7 @@ export const register = async(req,res,next) => {
             const options = {
                 expires : new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
                 httpOnly : true,
+                secure : true,
                 sameSite : "none", 
             }
 
@@ -76,7 +77,8 @@ export const login  = async(req,res) => {
                 console.log(' Login token is - ',token);
                  const options = {
                     expires : new Date(Date.now() +  15 * 24 * 60 * 60 * 1000),
-                    httpOnly : true,
+                    httpOnly : true, 
+                    secure : true,
                     sameSite : "none", 
                 }
     
@@ -110,7 +112,7 @@ export const getmyprofile  = async(req,res) => {
 
         res.status(200).json({
             message : " User Found",
-            getuser
+            getuser,
         })
     }catch(error)
     {
