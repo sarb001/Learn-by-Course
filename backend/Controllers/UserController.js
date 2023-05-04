b3d51ebb4fb11c538b95673ba9130dab7a4bff8b
SARB <mrsinghbusiness05@gmail.com>
2023-05-04 14:28:22 +0530

    Https- true

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -74,7 +74,7 @@ export const login  = async(req,res) => {
                 console.log(' Login token is - ',token);
                  const options = {
                     expires : new Date(Date.now() +  15 * 24 * 60 * 60 * 1000),
-                    httpOnly :false, 
+                    httpOnly :true, 
                     secure : true,
                     sameSite : "none", 
                 }
@@ -96,7 +96,7 @@ export const login  = async(req,res) => {
 export const logout = async(req,res) => {
     res.status(200).cookie("token",null , {
         expires : new Date(Date.now()),
-        httpOnly : false,
+        httpOnly : true,
         secure : true,
         sameSite : "none", 
     }).json({
