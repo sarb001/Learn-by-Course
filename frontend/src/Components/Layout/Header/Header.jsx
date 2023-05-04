d252fd7fceb2ac9e8dec0c1421c44ce3fb3d60b9
SARB <mrsinghbusiness05@gmail.com>
2023-05-04 05:45:56 +0530

    Text changed

--- frontend/src/Components/Layout/Header/Header.jsx
+++ frontend/src/Components/Layout/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = ({user, isAuthenticated }) => {
         <DrawerOverlay />
         <DrawerContent>
               <DrawerHeader borderBottomWidth={'1px'}>
-                COURSE BUNDLERRGG 
+                COURSE BUNDLERRGGJJ
               </DrawerHeader>
 
                 <DrawerBody>
