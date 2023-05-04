ccdd9a3434cceb0ea22aea5abfc553ee065d2244
SARB <mrsinghbusiness05@gmail.com>
2023-05-04 14:19:33 +0530

    Typing Pass Error

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -62,7 +62,7 @@ export const login  = async(req,res) => {
             let user = await User.findOne({email}).select("+password");
             if(!user) return res.json({message: " User not Present "})
             // const ismatch = await user.comparePassword(password);
-            const ismatch = await bcrypt.compare(password.user.password)
+            const ismatch = await bcrypt.compare(password,user.password)
 
             if(!ismatch){
                  return res.json({message : ' InCorrect Email or Password '});
