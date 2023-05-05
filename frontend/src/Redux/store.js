24562984ac295bbb85e510658e1623fc9e0aa9bc
SARB <mrsinghbusiness05@gmail.com>
2023-05-06 03:52:13 +0530

    Server URL changed

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useReducer } from './reducers/useReducer';
 
-export const server = "https://learn-by-course-production.up.railway.app/api/v1";
+export const server = "learn-by-course-production-1594.up.railway.app/api/v1";
 
 const store = configureStore({
     reducer : {
