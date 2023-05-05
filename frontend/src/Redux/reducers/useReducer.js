ffb490301f1b98d2f0c25b7ed6bb644306f3497e
SARB <mrsinghbusiness05@gmail.com>
2023-05-06 04:43:58 +0530

    Minorchange in loginreducer

--- frontend/src/Redux/reducers/useReducer.js
+++ frontend/src/Redux/reducers/useReducer.js
@@ -1,20 +1,19 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 export const useReducer =  createReducer({}, {
-
     loginRequest  : (state,action) =>  {
         state.loading = true;
     },
     loginSuccess  : (state,action) =>  {
         state.loading = false;
         state.isAuthenticated = true;
-        state.user = action.payload.user;
+        state.user = action.payload;
         state.message = action.payload.message     
     },
     loginFail  : (state,action) =>  {
         state.loading = false;
         state.isAuthenticated = false;
-        state.error = action.payload.error;
+        state.error = action.payload;
     },
     clearError : (state,action) => {
         state.error = null;
