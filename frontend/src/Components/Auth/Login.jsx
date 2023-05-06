cf07f4ec01afa73d61cae1fe519a010172a2a246
SARB <mrsinghbusiness05@gmail.com>
2023-05-06 10:07:16 +0530

    Login Bug Fixed

--- frontend/src/Components/Auth/Login.jsx
+++ frontend/src/Components/Auth/Login.jsx
@@ -12,7 +12,6 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import  { useDispatch } from 'react-redux';
 import { login } from '../../Redux/actions/user';
-// import { login } from '../../Redux/actions/user';
 
 const Login = () => {
   
