deb333d0385fd931e750f637064ba2e2831bdea3
SARB <mrsinghbusiness05@gmail.com>
2023-05-11 16:46:23 +0530

    SMTP_HOST name changed

--- backend/Utils/sendmail.js
+++ backend/Utils/sendmail.js
@@ -4,6 +4,7 @@ export const sendEmail = async(to,subject,text) => {
     const transporter  = createTransport({
         host : process.env.SMTP_HOST,
         port : process.env.SMTP_PORT,
+        secure : true,
         auth : {
             user : process.env.SMTP_USER,
             pass : process.env.SMTP_PASS,
