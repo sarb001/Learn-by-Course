380aa9613633e3a6a194b0415b700391cc670700
SARB <mrsinghbusiness05@gmail.com>
2023-05-12 06:47:22 +0530

    portnum. changed

--- backend/Utils/sendmail.js
+++ backend/Utils/sendmail.js
@@ -4,7 +4,7 @@ export const sendEmail = async(to,subject,text) => {
     const transporter  = createTransport({
         host : process.env.SMTP_HOST,
         port : process.env.SMTP_PORT,
-        secure : false,
+        secure : true,
         auth : {
             user : process.env.SMTP_USER,
             pass : process.env.SMTP_PASS,
