8d52e6f18fd328be6524fd7409cf0a559fb3a0a6
SARB <mrsinghbusiness05@gmail.com>
2023-05-12 07:34:57 +0530

    Directly acc. values

--- backend/Utils/sendmail.js
+++ backend/Utils/sendmail.js
@@ -2,12 +2,12 @@ import { createTransport } from 'nodemailer';
 
 export const sendEmail = async(to,subject,text) => {
     const transporter  = createTransport({
-        host : process.env.SMTP_HOST,
-        port : process.env.SMTP_PORT,
+        host : "smtp.gmail.com",
+        port : 465,
         secure : true,
         auth : {
-            user : process.env.SMTP_USER,
-            pass : process.env.SMTP_PASS,
+            user : "50751179a03882",
+            pass : "6f37c5e9548205",
         },
         debug : true,
     });
