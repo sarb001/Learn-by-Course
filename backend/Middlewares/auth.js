7970b9584ed74e911f03d8a50349251f9ca3039c
SARB <mrsinghbusiness05@gmail.com>
2023-05-15 13:36:12 +0530

    Middleware Added

--- backend/Middlewares/auth.js
+++ backend/Middlewares/auth.js
@@ -1,20 +1,20 @@
 
 import  { User }  from '../Models/User.js';
 import jwt from 'jsonwebtoken';
+import { catchAsyncError } from './catchAsyncError.js';
+import ErrorHandler from '../Utils/errorhandler.js';
 
-export const isAuthenticated = async(req,res,next) => {
+export const isAuthenticated =   catchAsyncError(async ( req,res,next) => {
     const { token } = req.cookies;
 
-    if(!token){
-        return res.status(401).json({message : " User Not Logged in Broo "})
-    }
+    if(!token) return next(new ErrorHandler(" Not Logged In ",401));
 
     const decoded = jwt.verify(token,'ekekkkeke');
     console.log('decoded data is --',decoded);
 
     req.user = await User.findById(decoded._id);
     next();
-}
+})
 
 export const authorizeAdmin = async(req,res,next) => {
     if(req.user.role !== "admin"){
